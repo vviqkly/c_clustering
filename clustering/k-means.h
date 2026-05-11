b541043eb8b0ee34f48d99fc4280bb09dbe51b71
Виктория Сергиенко <v.sergienko1@g.nsu.ru>
2026-05-11 18:22:17 +0700

    Структура для k-means + функция инициализации центроидов (рандом)

---
+++ clustering/k-means.h
@@ -0,0 +1,14 @@
+#ifndef KMEANS_H
+#define KMEANS_H
+
+#include "common.h"
+
+typedef struct {
+    Point *centroids;         // массив центроидов 
+    int k;                    // кол-во кластеров
+    int max_iter;             // мак кол-во итераций (для завершения)
+} KMeans;
+
+void kmeans_random(KMeans *km, Point *data, int n, int k);
+
+#endif
